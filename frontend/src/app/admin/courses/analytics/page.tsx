62a0ad0e7beff367bc1d2931d0ee298516493693
Saurabh Ojha <sourabhojha12@gmail.com>
2025-08-06 21:10:20 +0530

    create admin analytics pages

--- frontend/src/app/admin/courses/analytics/page.tsx
+++ frontend/src/app/admin/courses/analytics/page.tsx
@@ -1,10 +1,12 @@
-import UnderDevelopment from "@/components/ui/UnderDevelopment";
+import AdminTopHeader from "@/components/admin/AdminTopHeader";
+import Analytics from "@/components/admin/courses/Analytics";
 import React from "react";
 
 const AdminCourseAnalytics = () => {
   return (
-    <div className="w-full h-full flex-col gap-4 p-8">
-      <UnderDevelopment className="w-full h-full flex flex-col justify-center" />
+    <div className="w-full h-full flex-col gap-4">
+      <AdminTopHeader />
+      <Analytics />
     </div>
   );
 };
