e71e2af872391931253923d93605b7a6e9889229
AroraKaran19 <karora0319@gmail.com>
2025-08-27 00:47:40 +0530

    feat(home): add CoursesSection and TopCoursesSection components to the homepage (temporary)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,13 @@
+import CoursesSection from "./(pages)/courses/components/CoursesSection";
+import TopCoursesSection from "./(pages)/courses/components/TopCoursesSection";
 
 export default function Home() {
+
+  // Added Courses page in this (temporary)
   return (
-    <div>
-    </div>
+    <>
+      <TopCoursesSection />
+      <CoursesSection />
+    </>
   );
 }
