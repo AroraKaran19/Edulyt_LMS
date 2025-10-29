c5a289bd304a0d33827769a66d96df774c3c5aec
AroraKaran19 <karora0319@gmail.com>
2025-10-29 19:10:01 +0530

    fix: update CurriculumSection title from "Curriculum" to "Course Features" for improved clarity in course content presentation

--- frontend/src/app/(pages)/courses/[slug]/components/CurriculumSection.tsx
+++ frontend/src/app/(pages)/courses/[slug]/components/CurriculumSection.tsx
@@ -42,7 +42,7 @@ const CurriculumSection = ({ course }: { course: Course }) => {
 
   return (
     <SectionContainer id="curriculum">
-      <CourseTitle title="Curriculum" className="text-4xl text-text-primary" />
+      <CourseTitle title="Course Features" className="text-4xl text-text-primary" />
       {course.features && course.features.length > 0 && (
         <div className="course-features-container w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 select-none">
           {course.features.map((feature: string, index: number) => (
